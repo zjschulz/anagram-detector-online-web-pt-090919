9126b7132b6696543910ab813408f590cc1d0034
Zachary Schulz <zjschulz@gmail.com>
2019-10-06 19:16:58 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :detector
+  
+  def initialize(detector)
+    @detector = detector
+  end
+  
+  def match(word)
+    word.select do |x| x.split("").sort == @detector.split("").sort
+    end
+  end
+  
+end
