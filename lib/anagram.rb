c9fe15a5a40e7967addd41ff8005f2c59a30d736
Zachary Schulz <zjschulz@gmail.com>
2019-10-06 19:16:43 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,7 @@ class Anagram
   end
   
   def match(word)
-    word.each do |x| x.split("").sort == @detector.split("").sort
+    word.select do |x| x.split("").sort == @detector.split("").sort
     end
   end
   
